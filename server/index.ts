1f6341e896d6bd9eb4c6505c1a084e80834853a5
sandrockjustin <sandrock.justin@gmail.com>
2024-11-17 22:54:25 -0600

    fix / server - import Prisma Client && init connection

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from "dotenv";
+import database from './db/index.ts';
 
 //configure dotenv
 dotenv.config();
@@ -9,6 +10,7 @@ const app = express();
 const PORT: String = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
+
 // ROUTERS
 
 //
@@ -16,8 +18,16 @@ const CLIENT_URL = process.env.CLIENT_URL;
 app.use(express.static(path.resolve(__dirname, '../client/dist/')));
 
 app.listen(PORT, () => {
-    console.log("Server listening on Port",CLIENT_URL + ':' + PORT);
+    database.$connect()
+        .then((connectionEstablished) => {
+            console.log(`Prisma has connected to the database...`);
+            console.log("Server listening on Port",CLIENT_URL + ':' + PORT);
+        })
+        .catch((error) => {
+            console.error(`Failure on database connection...`)
+        })
 });
+
 app.on('error', (err: any) => console.error('Error', err));
 
 export default { app };
