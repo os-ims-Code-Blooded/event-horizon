3fdb201b1c31613fd69a81234fe59fc8d0906bde
sandrockjustin <sandrock.justin@gmail.com>
2024-12-04 18:52:07 -0600

    fix / misc server index changes

--- server/index.ts
+++ server/index.ts
@@ -213,6 +213,7 @@ io.on('connection', (socket)=>{
         console.log(`===================================================================`)
         console.log(`Two players have been detected for Game Session #${data}.`)
         console.log(`The Game Board should be rendered by the client for the session.`)
+        console.log(findPlayerInfo);
         console.log(`===================================================================\n`)        
       }
 
