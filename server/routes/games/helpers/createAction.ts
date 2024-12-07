d46bd0e54b5247e1f4866ec0910914c68422292e
sandrockjustin <sandrock.justin@gmail.com>
2024-12-06 19:48:56 -0600

    fix / on a createAction, deck is updated to reflect change in cards

--- server/routes/games/helpers/createAction.ts
+++ server/routes/games/helpers/createAction.ts
@@ -5,14 +5,11 @@ export default async function createAction(req: any){
   try {
 
     const cardSubmission = req.body.data.card_id ? req.body.data.card_id : null;
-    console.log("*** req.body.data.card_id ***\n", req.body.data.card_id)
 
     if (cardSubmission){
       const findCardCorrelation = await database.user_Cards.findFirst({ where: { id: Number(req.body.data.card_id) }})
       
       const cardDetails = await database.cards.findFirst({ where: { id: findCardCorrelation.card_id}})
-      
-      console.log("CARD ID MISMATCH", req.body.data.card_id, cardDetails.id)
 
       const newAction = await database.actions.create({
         data: {
@@ -22,6 +19,25 @@ export default async function createAction(req: any){
           action: req.body.data.action,
         }
       })
+
+      //============================ NEW CODE =======================//
+      /**
+       *  this code expects for a submitted action (each user) to
+       *  provide a string JSON of their current deck under property 
+       *  'deck' when they submit an action
+       */ 
+      await database.game_Card_States.updateMany({
+        where: {
+          AND: [
+            { round_id: req.body.data.round_id },
+            { user_id: req.body.data.user_id}
+          ]
+        },
+        data: {
+          deck: req.body.data.deck
+        }
+      })
+      //=============================================================//
 
       console.log(`createAction.ts : 11 | New action for User #${newAction.user_id} on Round #${newAction.round_id}; includes card #${newAction.card_id} with action type '${newAction.action}'.`);
       return newAction;
