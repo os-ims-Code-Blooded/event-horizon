32554a92e850ab4478759a8915694e7541caeaa2
sandrockjustin <sandrock.justin@gmail.com>
2024-12-07 14:45:49 -0600

    fix / reworked LOAD operations for gameplay

--- server/routes/games/helpers/commitLoad.ts
+++ server/routes/games/helpers/commitLoad.ts
@@ -1,4 +1,5 @@
 import database from "../../../db/index.ts";
+import shuffle from "./shuffle.ts";
 
 export default async function commitLoad(req: any, game: number, action: any){
 
@@ -17,8 +18,17 @@ export default async function commitLoad(req: any, game: number, action: any){
     // if must be fired, then load this shell
     if (isLethalPayload) {
 
-      console.log(`commitLoad.ts : 18 | Loading offensive Card #${payload.id} for User #${action.user_id} on Game #${game} - Round #${action.round_id}.`);
+      // create the damage action to store in actions_Loaded
+      const damageAction = await database.actions_Loaded.create({
+        data: {
+          game:   { connect: { id: game}},
+          round:  { connect: { id: action.round_id}},
+          action: { connect: { id: action.id, user_id: action.user_id}},
+          card:   { connect: { id: payload.id}},
+        }
+      })
 
+      // find out if they already have a card loaded
       const alreadyLoaded = await database.actions_Loaded.findFirst({
         where: {
           AND: [
@@ -31,16 +41,20 @@ export default async function commitLoad(req: any, game: number, action: any){
         }
       })
 
-
+      // if they are already loaded
       if (alreadyLoaded) {
 
+        // delete that entry from the actions that are loaded for them
         await database.actions_Loaded.deleteMany({
-          where: { user_id: action.user_id }
+          where: { 
+            AND: [
+              { user_id: action.user_id },
+              { game_id: game }
+            ]
+          }
         })
 
-        console.log(`A card was already loaded for user #${action.user_id}, the card was: `, alreadyLoaded.card);
-
-        // find the card state for this user
+        // find the card state for this user, including the hand and the deck
         const userDeck = await database.game_Card_States.findFirst({
           where: {
             AND: [
@@ -49,11 +63,12 @@ export default async function commitLoad(req: any, game: number, action: any){
             ]
           },
           select: {
-            deck: true
+            deck: true,
+            hand: true
           }
-        })
+        });
 
-        // find the user-card relationship for the card that they played (server expects this to be a User_Card relation when sent from client)
+        // find the user-card relationship for the card that they previously played (server expects this to be a User_Card relation when sent from client)
         const card = await database.user_Cards.findFirst({
           where: {
             AND: [
@@ -61,12 +76,20 @@ export default async function commitLoad(req: any, game: number, action: any){
               { card_id: alreadyLoaded.card_id }
             ]
           }
-        })
+        });
 
-        const localDeckState: any = userDeck.deck;
+        let localDeckState: any = userDeck.deck;
+        let localHandState: any = userDeck.hand;
 
+        // add the card back to the deck
         if (Array.isArray(localDeckState)) {
           localDeckState.push(card)
+        }
+
+        // filter hand to find the card that we are deleting, and exlcude it
+        if (Array.isArray(localHandState)){
+          localHandState = localHandState.filter((card) => card.card_id !== payload.id) // removes the card for this turn from the hand
+          localHandState.push(shuffle(localDeckState).pop());                           // push a random card to the hand
         }
 
         // update the stored deck with parsedDeck being turned back into a string
@@ -78,22 +101,84 @@ export default async function commitLoad(req: any, game: number, action: any){
             ]
           }, 
           data: {
-            deck: localDeckState
+            deck: localDeckState,
+            hand: localHandState
+          }
+        })
+        
+        // create the damage action to store in actions_Loaded
+        const damageAction = await database.actions_Loaded.create({
+          data: {
+            game:   { connect: { id: game}},
+            round:  { connect: { id: action.round_id}},
+            action: { connect: { id: action.id, user_id: action.user_id}},
+            card:   { connect: { id: payload.id}},
           }
         })
 
-      }
+      } else {
 
-      const damageAction = await database.actions_Loaded.create({
-        data: {
-          game:   { connect: { id: game}},
-          round:  { connect: { id: action.round_id}},
-          action: { connect: { id: action.id, user_id: action.user_id}},
-          card:   { connect: { id: payload.id}},
+        // find the card state for this user, including the hand and the deck
+        const userDeck = await database.game_Card_States.findFirst({
+          where: {
+            AND: [
+              { round_id: action.round_id},
+              { user_id: action.user_id }
+            ]
+          },
+          select: {
+            deck: true,
+            hand: true
+          }
+        });
+
+        // find the user-card relationship for the card that they previously played (server expects this to be a User_Card relation when sent from client)
+        const card = await database.user_Cards.findFirst({
+          where: {
+            AND: [
+              { user_id: action.user_id },
+              { card_id: alreadyLoaded.card_id }
+            ]
+          }
+        });
+
+        let localDeckState: any = userDeck.deck;
+        let localHandState: any = userDeck.hand;
+
+        // filter hand to find the card that we are deleting, and splice it
+        if (Array.isArray(localHandState)){
+          localHandState = localHandState.filter((card) => card.card_id !== payload.id) // removes the card for this turn from the hand
+
+          if (localDeckState.length > 0) {
+            localHandState.push(shuffle(localDeckState).pop());                         // push a random card to the hand
+          }
         }
-      })
 
-      console.log(`commitLoad.ts : 22 | Offensive Card #${damageAction.card_id} loaded for User #${damageAction.user_id} on Game #${damageAction.game_id} - Round #${damageAction.round_id}.`);
+        // update the stored deck with these changes
+        await database.game_Card_States.updateMany({
+          where: {
+            AND: [
+              { round_id: action.round_id},
+              { user_id: action.user_id }
+            ]
+          }, 
+          data: {
+            deck: localDeckState,
+            hand: localHandState
+          }
+        })
+
+        // create the damage action to store in actions_Loaded
+        const damageAction = await database.actions_Loaded.create({
+          data: {
+            game:   { connect: { id: game}},
+            round:  { connect: { id: action.round_id}},
+            action: { connect: { id: action.id, user_id: action.user_id}},
+            card:   { connect: { id: payload.id}},
+          }
+        })
+        
+      }
 
     // if this must not be fired, then just add it as an effect
     } else if (isNonLethalPayload) {
