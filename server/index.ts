956182d4218728490b0b05a9c3a0a1b23805b122
sandrockjustin <sandrock.justin@gmail.com>
2024-12-06 19:43:42 -0600

    fix / server has talking guns for round response

--- server/index.ts
+++ server/index.ts
@@ -258,17 +258,46 @@ io.on('connection', (socket)=>{
 
     console.log(" ENDED TURN DATA ", data)
 
-    try{
+    try {
       const response = await gameHandler(data)
-
       io.in(data.session).emit('received_rounds_data', response)
-
-
     }
     catch(err){
       console.error(err)
     }
 
+  })
+
+  socket.on('deck_state_request', async (data) => {
+
+    /*
+    data: {
+      user_id:   your user id,
+      socket_id: your socket id,
+      round_id:  your current round
+    }
+    */
+    try {
+
+      const deckState = await database.game_Card_States.findFirst({
+        where: {
+          AND: [
+            { user_id: Number(data.user_id) },
+            { round_id: Number(data.round_id)}
+          ]
+        }
+      })
+
+      const preformat = {
+        "Current Deck": deckState.deck,
+        "Current Hand": deckState.hand
+      }
+
+      io.to(data.socket_id).emit('deck_state_response', preformat)
+
+    } catch (error) {
+      console.error(`Failure on a self-identify emission from client.`)
+    }
   })
 ////////////////////////////////////////
 // PLAYER SELF-DESTRUCTS
