adfc6cee74dac40f199994d9017751ab81fbb6bc
Jeremy Hernandez <jeremy.hernandez504@gmail.com>
2024-12-17 17:17:43 -0600

    CSS/ Adjustments made to some components

--- client/components/Navigation.tsx
+++ client/components/Navigation.tsx
@@ -172,9 +172,14 @@ const NavigationBar: FC<NavProps> = ({isMuted, handleToggleMute, setVolume, clic
           </div>
         )}
       </div>
-
       {/* Theme Toggle Buttons */}
-      <div className="flex items-center space-x-2">
+      <div className=" flex flex-row items-center">
+      <button className="relative mx-4 text-slate-600 transition-colors duration-300 transform lg:block dark:text-slate-200 hover:text-slate-700 dark:hover:text-slate-400 focus:text-slate-700 dark:focus:text-slate-400 focus:outline-none hover:" aria-label="show notifications" >
+          <svg className=" w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          <span className="absolute top-0 right-0 items-center justify-center px-1 py-1 rounded-full bg-red-500 text-xs font-medium text-white"> </span> 
+      </button>
         {/* Volume button */}
         <button
           onClick={toggleVolSlider}
@@ -216,6 +221,7 @@ const NavigationBar: FC<NavProps> = ({isMuted, handleToggleMute, setVolume, clic
             </div>
           </div>
         )}
+        
         <button onClick={toggleDarkMode} className="hover:bg-orange-400 dark:hover:bg-purple-300 rounded-full">
           {isDarkMode ?
             <svg data-toggle-icon="moon" className="w-3.5 h-3.5 " xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
