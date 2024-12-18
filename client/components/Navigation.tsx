14d84c49a7df985849dfeb31fbef3285e61a739e
Jeremy Hernandez <jeremy.hernandez504@gmail.com>
2024-12-18 16:15:45 -0600

    Nav adjustments conditionally rendeing invite icon

--- client/components/Navigation.tsx
+++ client/components/Navigation.tsx
@@ -219,16 +219,18 @@ const NavigationBar: FC<NavProps> = ({setUserAcceptedInvs, setUserInvites, userI
       {/* Right Side icons div  */}
       <div className=" relative flex flex-row items-center">
         {/* Notification button alert*/}
-      <button className="mx-4 text-slate-600 transition-colors duration-300 transform lg:block dark:text-slate-200 hover:text-slate-700 dark:hover:text-slate-400 focus:text-slate-700 dark:focus:text-slate-400 focus:outline-none hover:" aria-label="show notifications"
-        onClick={notificationToggle}
-      >
-          <svg className="relative w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" stroke="currentColor"/>
-          </svg>
-          {userInvites.length > 0 && (
-            <span className="absolute top-0 right-0 items-center justify-center px-1 py-1 rounded-full bg-error text-xs font-medium text-white"> </span>
-          )}
-      </button>
+        {user && (
+          <button className="mx-4 text-slate-600 transition-colors duration-300 transform lg:block dark:text-slate-200 hover:text-slate-700 dark:hover:text-slate-400 focus:text-slate-700 dark:focus:text-slate-400 focus:outline-none hover:" aria-label="show notifications"
+            onClick={notificationToggle}
+          >
+              <svg className="relative w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" stroke="currentColor"/>
+              </svg>
+              {userInvites.length > 0 && (
+                <span className="absolute top-0 right-0 items-center justify-center px-1 py-1 rounded-full bg-error text-xs font-medium text-white"> </span>
+              )}
+          </button>
+        )}
         {/* Notification Window */}
       {inviteTableOpen && (
         <div className="absolute top-8 right-0 mt-2 w-72 bg-fifth dark:bg-third border border-slate-200 rounded-lg shadow-lg z-50">
