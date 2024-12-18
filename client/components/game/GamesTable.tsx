c9c4ee9a18f206c8e8b86c03e98329948e171a82
benlongcp <164564032+benlongcp@users.noreply.github.com>
2024-12-17 22:22:53 -0600

    games table formatting

--- client/components/game/GamesTable.tsx
+++ client/components/game/GamesTable.tsx
@@ -4,81 +4,96 @@ import axios from 'axios'
 
 export default function GameTable({
   
-  playHeavyClickSFX
+  playHeavyClickSFX,
+  userInvites,
+  setUserAcceptedInvs,
+  setUserInvites
 
 }){
 
 
+  console.log("!!! USER INVITES !!!", userInvites)
+
 ///////////////// RENDER RETURN //////////////////////////////
   return(
-<div className='bg-red-300'>
+<div className='bg-red-300 w-full'>
 
 
 
 <div className="relative overflow-x-auto">
 
-    <table className="w-full text-sm text-left rtl:text-right text-slate-500 dark:text-slate-400">
+
+
+
+
+
+    <table className="text-sm text-left rtl:text-right text-slate-500 dark:text-slate-400 px-2 justify-between">
+
+
 
         <thead className="text-xs text-white uppercase bg-slate-50 dark:bg-slate-700 dark:text-gray-400">
-            <tr>
+            <tr className='justify-between p-2'>
                 <th scope="col" className="px-6 py-3">
                     player name
                 </th>
                 <th scope="col" className="px-6 py-3">
                     status
                 </th>
-                <th>decline</th>
-            
+                <th>select game</th>
+                <th className='text-end pr-4'>decline</th>
+
             </tr>
         </thead>
+
+
+
         <tbody>
+
+
+        {userInvites.map((invite)=>{
+
+          return(
             <tr className="bg-gray border-b dark:bg-slate-800 dark:border-slate-700">
                 <th scope="row" className="px-6 py-4 font-medium text-slate-900 whitespace-nowrap dark:text-white">
-                    GuilelessLlama_734
+                    {invite.invitee.name}
                 </th>
                 <td className="px-6 py-4">
-                    pending
+                    {invite.accepted? <p>accepted</p>: <p>pending</p>}
                 </td>
                 <td className="px-6 py-4">
                 <button onClick={()=>{
                       playHeavyClickSFX()
                     }}
-                    className='w-8 h-8 aspect-square bg-red-600 hover:bg-red-900 text-text dark:text-darkText border-slate-600 border-2 font-bold text-xs sm:text-sm md:text-base lg:text-lg rounded-full flex justify-center items-center overflow-hidden text-ellipsis focus:ring-4 focus:ring-red-600'>X</button>
-                </td>
-            
-            </tr>
-            <tr className="bg-white border-b dark:bg-slate-800 dark:border-slate-700">
-                <th scope="row" className="px-6 py-4 font-medium text-slate-900 whitespace-nowrap dark:text-white">
-                    Blorkus
-                </th>
-                <td className="px-6 py-4">
-                    active
+                    className='w-8 h-8 aspect-square bg-success hover:bg-green-900 text-text dark:text-darkText border-slate-600 border-2 font-bold text-xs sm:text-sm md:text-base lg:text-lg rounded-full flex justify-center items-center overflow-hidden text-ellipsis focus:ring-4 focus:ring-success'>✓</button>
                 </td>
                 <td className="px-6 py-4">
                 <button onClick={()=>{
                       playHeavyClickSFX()
                     }}
-                    className='w-8 h-8 aspect-square bg-red-600 hover:bg-red-900 text-text dark:text-darkText border-slate-600 border-2 font-bold text-xs sm:text-sm md:text-base lg:text-lg rounded-full flex justify-center items-center overflow-hidden text-ellipsis focus:ring-4 focus:ring-red-600'>X</button>
+                    className='w-8 h-8 aspect-square bg-error hover:bg-red-900 text-text dark:text-darkText border-slate-600 border-2 font-bold text-xs sm:text-sm md:text-base lg:text-lg rounded-full flex justify-center items-center overflow-hidden text-ellipsis focus:ring-4 focus:ring-error'>X</button>
                 </td>
-           
             </tr>
-            <tr className="bg-white dark:bg-slate-800">
-                <th scope="row" className="px-6 py-4 font-medium text-slate-900 whitespace-nowrap dark:text-white">
-                    lefty_tighty_2020
-                </th>
-                <td className="px-6 py-4">
-                    active
-                </td>
-                <td className="px-6 py-4">
-                <button onClick={()=>{
-                      playHeavyClickSFX()
-                    }}
-                    className='w-8 h-8 aspect-square bg-red-600 hover:bg-red-900 text-text dark:text-darkText border-slate-600 border-2 font-bold text-xs sm:text-sm md:text-base lg:text-lg rounded-full flex justify-center items-center overflow-hidden text-ellipsis focus:ring-4 focus:ring-red-600'>X</button>
-                </td>
-          
-            </tr>
+
+          )
+
+        })}
+
+
+
+
+
         </tbody>
+
+
+
     </table>
+
+
+
+
+
+
+
 </div>
 
 
