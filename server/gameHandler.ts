7ba188336f78e69067a9f3e7660baa2a6c757f02
sandrockjustin <sandrock.justin@gmail.com>
2025-01-08 16:05:03 -0600

    fix / gameHandler now has a safety net for edge condition

--- server/gameHandler.ts
+++ server/gameHandler.ts
@@ -9,7 +9,7 @@ import { connect } from 'http2';
 import shuffle from './helpers/shuffle.ts';
 import errorHandler from './helpers/misc/error_logging/errorHandler.ts';
 
-export default async function gameHandler(req: any) {
+export default async function gameHandler(req: any, skip: boolean = false) {
 
   try {
 
@@ -28,7 +28,7 @@ export default async function gameHandler(req: any) {
     const playerHasAction = currentRound.actions.filter((action) => action.user_id === req.body.data.user_id)
 
     // if this is the first action submitted for a round
-    if (playerHasAction.length > 0) {
+    if (playerHasAction.length > 0 && !skip) {
       await createAction(req);
       return {
         "Success": true,
@@ -36,9 +36,54 @@ export default async function gameHandler(req: any) {
         "Waiting": true,
         "user_id": req.body.data.user_id
       }
-    } else if (currentRound.actions.length === 0){
+    } else if (currentRound.actions.length === 0 && !skip){
 
+      // we create an action
       await createAction(req);
+      
+      /**
+       * We initialize a safetyNet here, but what is this actually doing?
+       * Basically this handles the case that two players have submitted their turn,
+       * but it was done so quickly that the gameHandler did not process any information.
+       */
+      let safetyNet;
+
+      /**
+       * We check on an interval to see if two actions exist for this interaction.
+       */
+      let safetyInterval = setInterval(async () => {
+        try {
+          safetyNet = await database.rounds.findFirst({
+            where: { id: Number(req.body.data.round_id)},
+            include: {
+              round_effects: true,
+              game_player_information: true,
+              actions: true,
+              actions_loaded: true,
+            }
+          })
+
+          /**
+           * If two actions are found for the current round, only one iteration of the gameHandler
+           * will be retriggered through this code block.
+           */
+          if (safetyNet.actions.length === 2 && Number(req.body.data.user_id) === safetyNet.actions[0].user_id && !safetyNet.end_date) {
+            console.log(`Success on safety check, processing request now...`)
+            gameHandler(req, true);               // re-run the gameHandler
+            clearInterval(safetyInterval);        // clear the interval
+          } else if (safetyNet.end_date){
+            console.log(`Safety net does not need to occur at this time, round has ended...`);
+            clearInterval(safetyInterval);
+          } else {
+            console.log(`Initiating a safety interval check...`)
+          }
+
+        } catch (error) {
+          errorHandler(error);
+          console.error(`Error within safety net for gameHandler.`)
+        }
+      }, 15000)
+      
       return {
         "Success": true,
         "Message": `Action #1 created for User #${req.body.data.user_id} in Game #${currentRound.game_id} - Round #${currentRound.id}.`,
@@ -49,8 +94,10 @@ export default async function gameHandler(req: any) {
     // else if this is the second (and last) action submitted for a round
     } else if (currentRound.actions.length > 0){
 
-      // create the action for this user
-      await createAction(req);
+      if (!skip) {
+        // create the action for this user
+        await createAction(req);
+      }
 
       // then attempt to perform calculations, and store the results
       const updateState = await calculateGameState(req, currentRound.game_id);
@@ -109,7 +156,7 @@ export default async function gameHandler(req: any) {
 
   } catch (error) {
     errorHandler(error);
-    console.error(`Fatal error encountered within Rounds router (rounds.ts), error message follows: `, error);
+    console.error(`Fatal error encountered within gameHandler, error message follows: `, error);
     return {
       "Success": false,
       "Message": `Error on client in processing turn submission.`
